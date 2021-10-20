1c41bc5dc124948594af302ea5a2f865e86d50af
Juan A. Horrillo <jean.karin@gmail.com>
2021-10-20 11:52:57 +0200

    Añadimos sublime-merge

--- sublime.sh
+++ sublime.sh
@@ -4,4 +4,4 @@ curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --
 
 echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 
-sudo pacman -Syu sublime-text
+sudo pacman -Syu sublime-text sublime-marge --noconfirm
